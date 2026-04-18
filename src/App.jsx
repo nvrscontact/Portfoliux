c88507f5305debc6c6d9e19d0526b10a0967dc41
AlonsoSpain <alonungat@alu.edu.gva.es>
2026-04-18 18:59:59 +0200

    Responsive v2

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,46 @@ import Text from './components/Text'
 
 function App() {
 
+  /* 
+    technologies - parrafo - about 
+    languages - projectos - services
+
+
+    orden normal:
+    parrafo
+    projectscards
+    commintment
+    languages
+    technologies
+    focus
+    about
+    services
+    */
+
+
   return (
     <>
       <Navbar />
       <ButtonsTop title1={"Frontend Development"} title2={"Visual Prototypes"} title3={"Using UX / UI Techniques"} />
-      <section id="grid">
+      <section id="grid" className='contenedor'>
 
-        <HeroParagraph />
-        <ProjectsCards />
-        <section className='special_phrases'><Text title={"Commitment"} /></section>
-        <Languages />
-        <Technologies />
-        <section className='special_phrases'><Text title={"Focus"} /></section>
-        <About />
-        <Services />
+        <section className='one_sec'>
+          <Technologies />
+          <Languages />
+        </section>
+
+        <section className='two_sec'>
+          <HeroParagraph />
+          <ProjectsCards />
+        </section>
+
+        <section className='three_sec'>
+          <About />
+          <Services />
+        </section>
+
+        <section className='special_phrases commitment_order'><Text title={"Commitment"} /></section>
+        <section className='special_phrases focus_order'><Text title={"Focus"} /></section>
 
       </section>
       <Footer />
