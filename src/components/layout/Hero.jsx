b18487d1c46cf874282d6e9a592295818934b70f
AlonsoSpain <alonungat@alu.edu.gva.es>
2026-06-05 20:52:13 +0200

    g

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -1,13 +1,12 @@
-export default function Hero(){
+export default function Hero() {
     return (
         <div className="hero-sec">
-
-        <section className="hero-paragraph">
-            <img className="hero-img-behind" src="/img/test.svg" alt="" />
-            <h1>Websites with a <br /> visually 
-            <span className="special_relucient"> harmonious</span> 
-            <br /> <span className="special_relucient">design</span> that your <br /> customers <i>love</i></h1>
-        </section>
+            <section className="hero-paragraph">
+                <img className="hero-img-behind" src="/img/test2.svg" alt="" />
+                <h1>Websites with a <br /> visually
+                    <span className="special_relucient"> harmonious</span>
+                    <br /> <span className="special_relucient">design</span> that your <br /> customers <i>love</i></h1>
+            </section>
         </div>
     )
 }
